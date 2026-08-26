231ce5f57777b55fab464f6b0d907ec567e9b81a
Vijay Goud Gajjala <139239973+Vijay-goud@users.noreply.github.com>
2026-08-26 07:40:09 +0000

    Create application pages and component structure

---
+++ app/about/page.js
@@ -0,0 +1,8 @@
+export default function AboutPage(){
+    return(
+        <main>
+            <h1>About Travel Unbounded</h1>
+            <p>India's Most Trusted Experimental Travel Experts</p>
+        </main>
+    )
+}
---
+++ app/contact/page.js
@@ -0,0 +1,8 @@
+export default function ContactPage(){
+    return(
+    <main>
+        <h1>Plan Your Trip</h1>
+        <p>Tell us about your next adventure</p>
+    </main>
+    )
+}
